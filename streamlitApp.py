f2ecf4154df078de8ed432d89a74f5c6859893ef
Aishwarya U <91381783+AISHWARYAAU@users.noreply.github.com>
2024-09-25 14:37:55 +0530

    Update streamlitApp.py

--- streamlitApp.py
+++ streamlitApp.py
@@ -62,7 +62,7 @@ else:
             "The application will infer the one label out of 39 labels, as follows: 'Apple___Apple_scab', 'Apple___Black_rot', 'Apple___Cedar_apple_rust', 'Apple___healthy', 'Background_without_leaves', 'Blueberry___healthy', 'Cherry___healthy', 'Corn___Cercospora_leaf_spot Gray_leaf_spot', 'Corn___Common_rust', 'Corn___Northern_Leaf_Blight', 'Corn___healthy', 'Grape___Black_rot', 'Grape___Esca_(Black_Measles)', 'Grape___Leaf_blight_(Isariopsis_Leaf_Spot)', 'Grape___healthy', 'Orange___Haunglongbing_(Citrus_greening)', 'Peach___Bacterial_spot', 'Peach___healthy', 'Pepper,_bell___Bacterial_spot', 'Pepper,_bell___healthy', 'Potato___Early_blight', 'Potato___Late_blight', 'Potato___healthy', 'Raspberry___healthy', 'Soybean___healthy', 'Squash___Powdery_mildew', 'Strawberry___Leaf_scorch', 'Strawberry___healthy', 'Tomato___Bacterial_spot', 'Tomato___Early_blight', 'Tomato___Late_blight', 'Tomato___Leaf_Mold', 'Tomato___Septoria_leaf_spot', 'Tomato___Spider_mites Two-spotted_spider_mite', 'Tomato___Target_Spot', 'Tomato___Tomato_Yellow_Leaf_Curl_Virus', 'Tomato___Tomato_mosaic_virus', 'Tomato___healthy'."
         )
 
-    classes = [
+       classes = [
         "Apple___Apple_scab",
         "Apple___Black_rot",
         "Apple___Cedar_apple_rust",
@@ -103,7 +103,7 @@ else:
         "Tomato___healthy",
         "Background_without_leaves",
     ]
-
+    
     classes_and_descriptions = {
         "Apple___Apple_scab": "Apple with Apple scab disease detected.",
         "Apple___Black_rot": "Apple with Black rot disease detected.",
@@ -133,48 +133,79 @@ else:
         "Squash___Powdery_mildew": "Squash with Powdery mildew disease detected.",
         "Strawberry___Leaf_scorch": "Strawberry with Leaf scorch disease detected.",
         "Strawberry___healthy": "Healthy strawberry leaf detected.",
-        "Tomato___Bacterial_spot": "Tomato with Bacterial spot disease detected.",
-        "Tomato___Early_blight": "Tomato with Early blight disease detected.",
-        "Tomato___Late_blight": "Tomato with Late blight disease detected.",
-        "Tomato___Leaf_Mold": "Tomato with Leaf Mold disease detected.",
-        "Tomato___Septoria_leaf_spot": "Tomato with Septoria leaf spot disease detected.",
-        "Tomato___Spider_mites Two-spotted_spider_mite": "Tomato with Spider mites (Two-spotted spider mite) disease detected.",
-        "Tomato___Target_Spot": "Tomato with Target Spot disease detected.",
-        "Tomato___Tomato_Yellow_Leaf_Curl_Virus": "Tomato with Tomato Yellow Leaf Curl Virus disease detected.",
-        "Tomato___Tomato_mosaic_virus": "Tomato with Tomato mosaic virus disease detected.",
+        "Tomato___Bacterial_spot": "Tomato leaf with Bacterial spot disease detected.",
+        "Tomato___Early_blight": "Tomato leaf with Early blight disease detected.",
+        "Tomato___Late_blight": "Tomato leaf with Late blight disease detected.",
+        "Tomato___Leaf_Mold": "Tomato leaf with Leaf Mold disease detected.",
+        "Tomato___Septoria_leaf_spot": "Tomato leaf with Septoria leaf spot disease detected.",
+        "Tomato___Spider_mites Two-spotted_spider_mite": "Tomato leaf with Spider mites or Two-spotted spider mite disease detected.",
+        "Tomato___Target_Spot": "Tomato leaf with Target Spot disease detected.",
+        "Tomato___Tomato_Yellow_Leaf_Curl_Virus": "Tomato leaf with Tomato Yellow Leaf Curl Virus disease detected.",
+        "Tomato___Tomato_mosaic_virus": "Tomato leaf with Tomato mosaic virus disease detected.",
         "Tomato___healthy": "Healthy tomato leaf detected.",
-        "Background_without_leaves": "Background without leaves detected.",
+        "Background_without_leaves": "No plant leaf detected in the image.",
     }
-
-    # Load model
-    model = load_learner('./models/export.pkl', 'export.pkl')
-
-    # Image upload
+    
+    
+    # Define the functions to load images
+    def load_uploaded_image(file):
+        file_bytes = np.asarray(bytearray(file.read()), dtype=np.uint8)
+        opencv_image = cv2.imdecode(file_bytes, 1)
+        return opencv_image
+    
+    
+    # Set up the sidebar
     st.subheader("Select Image Input Method")
-    input_method = st.radio("Options", ["File Uploader", "Camera Input"], label_visibility="collapsed")
-
+    input_method = st.radio(
+        "options", ["File Uploader", "Camera Input"], label_visibility="collapsed"
+    )
+    
     # Check which input method was selected
     if input_method == "File Uploader":
-        uploaded_file = st.file_uploader("Choose an image file", type=["jpg", "jpeg", "png"])
+        uploaded_file = st.file_uploader(
+            "Choose an image file", type=["jpg", "jpeg", "png"]
+        )
         if uploaded_file is not None:
-            # Load and display the uploaded image
             uploaded_file_img = load_uploaded_image(uploaded_file)
             st.image(uploaded_file_img, caption="Uploaded Image", width=300)
             st.success("Image uploaded successfully!")
         else:
             st.warning("Please upload an image file.")
-
+    
     elif input_method == "Camera Input":
         st.warning("Please allow access to your camera.")
         camera_image_file = st.camera_input("Click an Image")
         if camera_image_file is not None:
-            # Load and display the camera input image
             camera_file_img = load_uploaded_image(camera_image_file)
             st.image(camera_file_img, caption="Camera Input Image", width=300)
             st.success("Image clicked successfully!")
         else:
             st.warning("Please click an image.")
-
+    
+    # model file path
+    export_file_path = "./models/export.pkl"
+    
+    
+    def Plant_Disease_Detection(img_file_path):
+        model = load_learner(export_file_path, "export.pkl")
+        # Get prediction and confidence score
+        prediction, idx, probabilities = model.predict(img_file_path)
+        
+        # Extract the confidence score
+        confidence_score = probabilities[idx].item()
+    
+        # Check if prediction is valid
+        if prediction not in classes:
+            prediction_sentence = f"The uploaded image is {prediction}, which is not compatible with the application. Please upload an image of a plant leaf for disease detection."
+            return prediction_sentence, None
+    
+        # Generate prediction message with confidence score
+        prediction_sentence = classes_and_descriptions[prediction]
+        confidence_message = f"Confidence: {confidence_score * 100:.2f}%"
+        
+        return prediction_sentence, confidence_message
+    
+    
     submit = st.button(label="Submit Leaf Image")
     if submit:
         st.subheader("Output")
@@ -182,21 +213,21 @@ else:
             img_file_path = uploaded_file_img
         elif input_method == "Camera Input":
             img_file_path = camera_file_img
-
+    
         prediction, confidence_message = Plant_Disease_Detection(img_file_path)
         with st.spinner(text="This may take a moment..."):
             st.write(prediction)
             if confidence_message:
                 st.write(confidence_message)
-
-footer = """
-<div style="text-align: center; font-size: medium; margin-top:50px;">
-    If you find ChromaticScan useful or interesting, please consider starring it on GitHub.
-    <hr>
-    <a href="https://github.com/SaiJeevanPuchakayala/ChromaticScan" target="_blank">
-    <img src="https://img.shields.io/github/stars/SaiJeevanPuchakayala/ChromaticScan.svg?style=social" alt="GitHub stars">
-  </a>
-</div>
-"""
-
-st.markdown(footer, unsafe_allow_html=True)
+    
+    footer = """
+    <div style="text-align: center; font-size: medium; margin-top:50px;">
+        If you find ChromaticScan useful or interesting, please consider starring it on GitHub.
+        <hr>
+        <a href="https://github.com/SaiJeevanPuchakayala/ChromaticScan" target="_blank">
+        <img src="https://img.shields.io/github/stars/SaiJeevanPuchakayala/ChromaticScan.svg?style=social" alt="GitHub stars">
+      </a>
+    </div>
+    """
+    
+    st.markdown(footer, unsafe_allow_html=True)
