58fb3aab0c1c7dea9c7d12395b04a387cd463b32
Aishwarya U <91381783+AISHWARYAAU@users.noreply.github.com>
2024-09-25 14:25:13 +0530

    Update streamlitApp.py

--- streamlitApp.py
+++ streamlitApp.py
@@ -9,9 +9,8 @@ from fastai import *
 from fastai.vision import *
 from fastai.imports import *
 from fastai.vision.all import *
-import cv2
-
 import pathlib
+import platform
 
 plt = platform.system()
 if plt == "Windows":
@@ -22,7 +21,6 @@ np.set_printoptions(suppress=True)
 
 # Set up the page layout
 st.set_page_config(page_title="ChromaticScan", page_icon=":camera:")
-
 
 st.title("ChromaticScan")
 
@@ -37,15 +35,27 @@ with st.sidebar:
     st.image(img)
     st.subheader("About ChromaticScan")
     st.write(
-        "ChromaticScan is a state-of-the-art convolutional neural network (CNN) algorithm that is specifically designed for detecting plant diseases. It utilizes transfer learning by fine-tuning the ResNet 34 model on a large dataset of leaf images to achieve an impressive 99.2% accuracy in detecting various plant diseases. The algorithm is trained to identify specific patterns and features in the leaf images that are indicative of different types of diseases, such as leaf spots, blights, and wilts."
+        "ChromaticScan is a state-of-the-art convolutional neural network (CNN) algorithm that is specifically designed for detecting plant diseases. "
+        "It utilizes transfer learning by fine-tuning the ResNet 34 model on a large dataset of leaf images to achieve an impressive 99.2% accuracy in detecting various plant diseases. "
+        "The algorithm is trained to identify specific patterns and features in the leaf images that are indicative of different types of diseases, such as leaf spots, blights, and wilts."
     )
 
     st.write(
-        "ChromaticScan is designed to be highly robust and accurate, with the ability to detect plant diseases in a wide range of conditions and environments. It can be used to quickly and accurately diagnose plant diseases, allowing farmers and gardeners to take immediate action to prevent the spread of the disease and minimize crop losses. With its high level of accuracy and ease of use, ChromaticScan is poised to revolutionize the way plant diseases are detected and managed in the agricultural industry."
+        "ChromaticScan is designed to be highly robust and accurate, with the ability to detect plant diseases in a wide range of conditions and environments. "
+        "It can be used to quickly and accurately diagnose plant diseases, allowing farmers and gardeners to take immediate action to prevent the spread of the disease and minimize crop losses. "
+        "With its high level of accuracy and ease of use, ChromaticScan is poised to revolutionize the way plant diseases are detected and managed in the agricultural industry."
     )
 
     st.write(
-        "The application will infer the one label out of 39 labels, as follows: 'Apple___Apple_scab', 'Apple___Black_rot', 'Apple___Cedar_apple_rust', 'Apple___healthy', 'Background_without_leaves', 'Blueberry___healthy', 'Cherry___healthy', 'Corn___Cercospora_leaf_spot Gray_leaf_spot', 'Corn___Common_rust', 'Corn___Northern_Leaf_Blight', 'Corn___healthy', 'Grape___Black_rot', 'Grape___Esca_(Black_Measles)', 'Grape___Leaf_blight_(Isariopsis_Leaf_Spot)', 'Grape___healthy', 'Orange___Haunglongbing_(Citrus_greening)', 'Peach___Bacterial_spot', 'Peach___healthy', 'Pepper,_bell___Bacterial_spot', 'Pepper,_bell___healthy', 'Potato___Early_blight', 'Potato___Late_blight', 'Potato___healthy', 'Raspberry___healthy', 'Soybean___healthy', 'Squash___Powdery_mildew', 'Strawberry___Leaf_scorch', 'Strawberry___healthy', 'Tomato___Bacterial_spot', 'Tomato___Early_blight', 'Tomato___Late_blight', 'Tomato___Leaf_Mold', 'Tomato___Septoria_leaf_spot', 'Tomato___Spider_mites Two-spotted_spider_mite', 'Tomato___Target_Spot', 'Tomato___Tomato_Yellow_Leaf_Curl_Virus', 'Tomato___Tomato_mosaic_virus', 'Tomato___healthy'."
+        "The application will infer the one label out of 39 labels, as follows: 'Apple___Apple_scab', 'Apple___Black_rot', 'Apple___Cedar_apple_rust', "
+        "'Apple___healthy', 'Background_without_leaves', 'Blueberry___healthy', 'Cherry___healthy', 'Corn___Cercospora_leaf_spot Gray_leaf_spot', "
+        "'Corn___Common_rust', 'Corn___Northern_Leaf_Blight', 'Corn___healthy', 'Grape___Black_rot', 'Grape___Esca_(Black_Measles)', "
+        "'Grape___Leaf_blight_(Isariopsis_Leaf_Spot)', 'Grape___healthy', 'Orange___Haunglongbing_(Citrus_greening)', 'Peach___Bacterial_spot', "
+        "'Peach___healthy', 'Pepper,_bell___Bacterial_spot', 'Pepper,_bell___healthy', 'Potato___Early_blight', 'Potato___Late_blight', "
+        "'Potato___healthy', 'Raspberry___healthy', 'Soybean___healthy', 'Squash___Powdery_mildew', 'Strawberry___Leaf_scorch', "
+        "'Strawberry___healthy', 'Tomato___Bacterial_spot', 'Tomato___Early_blight', 'Tomato___Late_blight', 'Tomato___Leaf_Mold', "
+        "'Tomato___Septoria_leaf_spot', 'Tomato___Spider_mites Two-spotted_spider_mite', 'Tomato___Target_Spot', 'Tomato___Tomato_Yellow_Leaf_Curl_Virus', "
+        "'Tomato___Tomato_mosaic_virus', 'Tomato___healthy'."
     )
 
 classes = [
@@ -90,10 +100,9 @@ classes = [
     "Background_without_leaves",
 ]
 
-
 classes_and_descriptions = {
     "Apple___Apple_scab": "Apple with Apple scab disease detected.",
-    "Apple___Black_rot": "Apple with Black rot disease detected",
+    "Apple___Black_rot": "Apple with Black rot disease detected.",
     "Apple___Cedar_apple_rust": "Apple with Cedar apple rust disease detected.",
     "Apple___healthy": "Healthy apple leaf detected.",
     "Blueberry___healthy": "Healthy blueberry leaf detected.",
@@ -114,8 +123,8 @@ classes_and_descriptions = {
     "Pepper,_bell___healthy": "Healthy bell pepper leaf detected.",
     "Potato___Early_blight": "Potato with Early blight disease detected.",
     "Potato___Late_blight": "Potato with Late blight disease detected.",
-    "Potato___healthy": "Healthy potato leaf detected",
-    "Raspberry___healthy": "Healthy raspberry leaf detected",
+    "Potato___healthy": "Healthy potato leaf detected.",
+    "Raspberry___healthy": "Healthy raspberry leaf detected.",
     "Soybean___healthy": "Healthy soybean leaf detected.",
     "Squash___Powdery_mildew": "Squash with Powdery mildew disease detected.",
     "Strawberry___Leaf_scorch": "Strawberry with Leaf scorch disease detected.",
@@ -133,13 +142,11 @@ classes_and_descriptions = {
     "Background_without_leaves": "No plant leaf detected in the image.",
 }
 
-
 # Define the functions to load images
 def load_uploaded_image(file):
     file_bytes = np.asarray(bytearray(file.read()), dtype=np.uint8)
     opencv_image = cv2.imdecode(file_bytes, 1)
     return opencv_image
-
 
 # Set up the sidebar
 st.subheader("Select Image Input Method")
@@ -149,9 +156,7 @@ input_method = st.radio(
 
 # Check which input method was selected
 if input_method == "File Uploader":
-    uploaded_file = st.file_uploader(
-        "Choose an image file", type=["jpg", "jpeg", "png"]
-    )
+    uploaded_file = st.file_uploader("Choose an image file", type=["jpg", "jpeg", "png"])
     if uploaded_file is not None:
         uploaded_file_img = load_uploaded_image(uploaded_file)
         st.image(uploaded_file_img, caption="Uploaded Image", width=300)
@@ -172,16 +177,14 @@ elif input_method == "Camera Input":
 # model file path
 export_file_path = "./models/export.pkl"
 
-
 def Plant_Disease_Detection(_img_file_path):
     model = load_learner(export_file_path, "export.pkl")
-    prediction = model.predict(img_file_path)[0]
+    prediction = model.predict(_img_file_path)[0]
     if prediction not in classes:
         prediction_sentence = f"The uploaded image is {prediction}, which is not compatible with the application. Please upload an image of a plant leaf for disease detection."
         return prediction_sentence
     prediction_sentence = classes_and_descriptions[prediction]
     return prediction_sentence
-
 
 submit = st.button(label="Submit Leaf Image")
 if submit:
@@ -190,37 +193,45 @@ if submit:
         img_file_path = uploaded_file_img
     elif input_method == "Camera Input":
         img_file_path = camera_file_img
-   predicted_class, confidence = Plant_Disease_Detection(img_file_path)
+
+    predicted_class, confidence = Plant_Disease_Detection(img_file_path)
     with st.spinner(text="This may take a moment..."):
-        st.write( predicted_class)
-        
-            if predicted_class:
-                st.write(f"Prediction: {predicted_class}")
-                st.write(f"Description: {classes_and_descriptions.get(predicted_class, 'No description available.')}")
-                st.write(f"Confidence: {confidence:.2f}%")
-                
-                # Prepare data for the table
-                recommendation = remedies.get(predicted_class, 'No recommendation available.')
-                status = "Unhealthy" if "healthy" not in predicted_class else "Healthy"
-                
-                data = {
-                    "Details": ["Leaf Status", "Disease Name", "Recommendation", "Accuracy"],
-                    "Values": [status, 
-                               predicted_class.split('___')[1] if "___" in predicted_class else 'Healthy',
-                               recommendation,
-                               f"{confidence:.2f}%"]
-                }
-                df = pd.DataFrame(data)
-                st.table(df)
-                 # Visualization: Pie Chart for Confidence
-                fig, ax = plt.subplots()
-                ax.pie([confidence, 100-confidence], labels=[f'Confidence: {confidence:.2f}%', ''], autopct='%1.1f%%', colors=['#4CAF50', '#D3D3D3'], startangle=90)
-                ax.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
-                st.pyplot(fig)
-            else:
-                st.error("Error in prediction. Please try again.")
-    elif submit:
-        st.warning("Please upload or capture an image first.")
+        if predicted_class:
+            st.write(f"Prediction: {predicted_class}")
+            st.write(f"Description: {classes_and_descriptions.get(predicted_class, 'No description available.')}")
+            st.write(f"Confidence: {confidence:.2f}%")
+            
+            # Prepare data for the table
+            recommendation = remedies.get(predicted_class, 'No recommendation available.')
+            status = "Unhealthy" if "healthy" not in predicted_class else "Healthy"
+            
+            data = {
+                "Details": ["Leaf Status", "Disease Name", "Recommendation", "Accuracy"],
+                "Values": [
+                    status,
+                    predicted_class.split('___')[1] if "___" in predicted_class else 'Healthy',
+                    recommendation,
+                    f"{confidence:.2f}%"
+                ]
+            }
+            df = pd.DataFrame(data)
+            st.table(df)
+            
+            # Visualization: Pie Chart for Confidence
+            fig, ax = plt.subplots()
+            ax.pie(
+                [confidence, 100-confidence],
+                labels=[f'Confidence: {confidence:.2f}%', ''],
+                autopct='%1.1f%%',
+                colors=['#4CAF50', '#D3D3D3'],
+                startangle=90
+            )
+            ax.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+            st.pyplot(fig)
+        else:
+            st.error("Error in prediction. Please try again.")
+else:
+    st.warning("Please upload or capture an image first.")
 
 footer = """
 <div style="text-align: center; font-size: medium; margin-top:50px;">
